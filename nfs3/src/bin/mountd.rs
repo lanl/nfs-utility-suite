57932c4d2cabfd9977340074551ce53c914e8d4a
Thomas Bertschinger <bertschinger@lanl.gov>
2025-07-11 15:47:17 -0600

    nfs3: mount protocol client and server stubs

--- nfs3/src/bin/mountd.rs
+++ nfs3/src/bin/mountd.rs
@@ -1,1 +1,52 @@
-fn main() {}
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright 2025. Triad National Security, LLC.
+
+use std::net::TcpListener;
+
+use rpc_protocol::server::*;
+use rpc_protocol::CallBody;
+
+use nfs3::mount::mount_proto::procedures::*;
+use nfs3::mount::mount_proto::*;
+
+struct MountState {
+    exports: Exports,
+}
+
+impl MountState {
+    fn new() -> Self {
+        Self {
+            exports: Exports {
+                inner: vec![ExportNode {
+                    dir: "/test/nfs/export".into(),
+                    groups: Groups {
+                        inner: vec![GroupNode {
+                            name: "localhost".into(),
+                        }],
+                    },
+                }],
+            },
+        }
+    }
+}
+
+fn main() {
+    let procedures: Vec<Option<RpcProcedure<MountState>>> = vec![
+        None,
+        None, // mount
+        None, // dump
+        None, // umount
+        None, // umountall
+        Some(export),
+    ];
+
+    let state = MountState::new();
+    let mut server = RpcService::new(MOUNT_PROGRAM, MOUNT_V3::VERSION, procedures, state);
+
+    let listener = TcpListener::bind("0.0.0.0:20048").unwrap();
+    server.run_blocking_tcp_server(listener);
+}
+
+fn export(_call: &CallBody, _arg: &[u8], state: &mut MountState) -> RpcResult {
+    RpcResult::Success(state.exports.serialize_alloc())
+}
---
+++ nfs3/src/lib.rs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright 2025. Triad National Security, LLC.
+
+pub mod mount {
+    include!(concat!(env!("OUT_DIR"), "/mount_proto.rs"));
+}
