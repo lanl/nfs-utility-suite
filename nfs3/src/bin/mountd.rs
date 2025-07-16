e1c93b0eeace028069d386a95f37072ac160a116
Thomas Bertschinger <bertschinger@lanl.gov>
2025-07-16 12:12:35 -0600

    nfs3: call SET rpc to load mountd address into RPCBIND server

--- nfs3/src/bin/mountd.rs
+++ nfs3/src/bin/mountd.rs
@@ -3,6 +3,7 @@
 
 use std::net::TcpListener;
 
+use rpc_protocol::rpcbind;
 use rpc_protocol::server::*;
 use rpc_protocol::CallBody;
 
@@ -40,13 +41,40 @@ fn main() {
         Some(export),
     ];
 
-    let state = MountState::new();
-    let mut server = RpcService::new(MOUNT_PROGRAM, MOUNT_V3::VERSION, procedures, state);
+    let handle = std::thread::spawn(|| {
+        let state = MountState::new();
+        let mut server = RpcService::new(MOUNT_PROGRAM, MOUNT_V3::VERSION, procedures, state);
 
-    let listener = TcpListener::bind("0.0.0.0:20048").unwrap();
-    server.run_blocking_tcp_server(listener);
+        let listener = TcpListener::bind("0.0.0.0:20048").unwrap();
+        server.run_blocking_tcp_server(listener);
+    });
+
+    if let Err(e) = announce_self() {
+        eprintln!("Could not set mountd address in RPCBIND server: {e}");
+        return;
+    }
+
+    let _ = handle.join();
 }
 
 fn export(_call: &CallBody, _arg: &[u8], state: &mut MountState) -> RpcResult {
     RpcResult::Success(state.exports.serialize_alloc())
+}
+
+/// Tell the RPCBIND server that the mount service is now running:
+fn announce_self() -> Result<(), rpc_protocol::Error> {
+    let service = rpcbind::RpcService {
+        prog: MOUNT_PROGRAM,
+        vers: MOUNT_V3::VERSION,
+        netid: "tcp".into(),
+        addr: "0.0.0.0.78.80".into(),
+        owner: "superuser".into(),
+    };
+
+    rpcbind::client::set(
+        service,
+        rpcbind::RpcbindServerAddress::Tcp("0.0.0.0:111".to_string()),
+    )?;
+
+    Ok(())
 }
