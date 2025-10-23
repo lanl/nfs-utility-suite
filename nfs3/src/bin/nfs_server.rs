d743bf282b3ea26e3f1d85f7651a5830674c790e
Thomas Bertschinger <bertschinger@lanl.gov>
2025-10-23 09:58:51 -0400

    nfs3: add command line args to nfsv3 server

--- nfs3/src/bin/nfs_server.rs
+++ nfs3/src/bin/nfs_server.rs
@@ -1,23 +1,33 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright 2025. Triad National Security, LLC.
 
+use clap::Parser;
+
 use rpc_protocol::{server::ring::*, server::RpcResult, CallBody};
 
 use ::nfs3::nfs3_xdr::{procedures::*, *};
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(long, default_value_t = 2049)]
+    port: u16,
+}
 
 struct ServerState {}
 
 fn main() {
     env_logger::init();
 
-    let procedures: Vec<Option<RingProcedure<ServerState>>> = vec![None, Some(getattr)];
+    let args = Cli::parse();
+    let address = format!("127.0.0.1:{}", args.port);
 
     let state = ServerState {};
 
+    let procedures: Vec<Option<RingProcedure<ServerState>>> = vec![None, Some(getattr)];
     let procedure_map =
         ProcedureMap::new(NFS_PROGRAM, NFS_V3::VERSION, NFS_V3::VERSION, procedures);
 
-    let mut server = RpcServer::new("127.0.0.1:2049", procedure_map, state).unwrap();
+    let mut server = RpcServer::new(&address, procedure_map, state).unwrap();
 
     server.main_loop().unwrap();
 }
